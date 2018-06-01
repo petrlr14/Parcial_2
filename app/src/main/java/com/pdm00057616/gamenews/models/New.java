f451f9bf7289e8eddaeeff2a88b79183a537a9cd
petrlr14 <00057616@uca.edu.sv>
2018-06-01 08:00:56 -0600

    se añadieron las clases modelo

---
+++ app/src/main/java/com/pdm00057616/gamenews/models/New.java
@@ -0,0 +1,61 @@
+package com.pdm00057616.gamenews.models;
+
+public class New {
+
+    private String _id, title, body, game, created_date;
+    private int __v;
+
+    @Override
+    public String toString() {
+        return title + "--" + game;
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public String getGame() {
+        return game;
+    }
+
+    public String getCreated_date() {
+        return created_date;
+    }
+
+    public int get__v() {
+        return __v;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public void setGame(String game) {
+        this.game = game;
+    }
+
+    public void setCreated_date(String created_date) {
+        this.created_date = created_date;
+    }
+
+    public void set__v(int __v) {
+        this.__v = __v;
+    }
+
+}
---
+++ app/src/main/java/com/pdm00057616/gamenews/models/User.java
@@ -0,0 +1,67 @@
+package com.pdm00057616.gamenews.models;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class User {
+    @SerializedName("_id")
+    private String id;
+    @SerializedName("created_date")
+    private String date;
+    @SerializedName("__v")
+    private int v;
+    @SerializedName("favoriteNews")
+    private List<New> favNews;
+
+    private String user;
+    private String password;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getV() {
+        return v;
+    }
+
+    public void setV(int v) {
+        this.v = v;
+    }
+
+    public List<New> getFavNews() {
+        return favNews;
+    }
+
+    public void setFavNews(List<New> favNews) {
+        this.favNews = favNews;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
