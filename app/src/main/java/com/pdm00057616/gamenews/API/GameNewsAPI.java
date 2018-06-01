fda9ec8f51d2909fe5bc24945fc771657545e18a
petrlr14 <00057616@uca.edu.sv>
2018-06-01 10:03:06 -0600

    se agrego GET para obtener usuarios

--- app/src/main/java/com/pdm00057616/gamenews/API/GameNewsAPI.java
+++ app/src/main/java/com/pdm00057616/gamenews/API/GameNewsAPI.java
@@ -1,10 +1,20 @@
 package com.pdm00057616.gamenews.API;
 
+import com.pdm00057616.gamenews.models.New;
+import com.pdm00057616.gamenews.models.User;
+
+import java.util.List;
+
+import io.reactivex.Single;
+import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface GameNewsAPI {
-/*
+
     @GET("/news")
-    Single<List<>>*/
+    Call<List<New>> getNews();
+
+    @GET("/users")
+    Call<List<User>> getUser();
 
 }
