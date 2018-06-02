6e384524dd036d421edcfdc8beb637b73e2239c1
petrlr14 <00057616@uca.edu.sv>
2018-06-02 10:24:04 -0600

    se preparó actividad con drawerlayout

--- app/src/main/java/com/pdm00057616/gamenews/MainActivity.java
+++ app/src/main/java/com/pdm00057616/gamenews/MainActivity.java
@@ -37,7 +37,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bindViews();
-        getToken();
     }
 
     @Override
