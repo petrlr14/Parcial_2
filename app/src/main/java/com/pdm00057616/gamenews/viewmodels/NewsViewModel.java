0d22d9ff1bb11fa83667835664acda2fc68db80d
petrlr14 <00057616@uca.edu.sv>
2018-06-03 17:06:13 -0600

    se agrego viewmodel que trabaja con la base local

--- app/src/main/java/com/pdm00057616/gamenews/viewmodels/NewsViewModel.java
+++ app/src/main/java/com/pdm00057616/gamenews/viewmodels/NewsViewModel.java
@@ -2,12 +2,30 @@ package com.pdm00057616.gamenews.viewmodels;
 
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
 import android.support.annotation.NonNull;
+
+import com.pdm00057616.gamenews.database.entities_models.NewEntity;
+import com.pdm00057616.gamenews.database.repositories.NewRepository;
+import com.pdm00057616.gamenews.models.New;
+
+import java.util.List;
 
 public class NewsViewModel extends AndroidViewModel {
 
+    private NewRepository repository;
 
     public NewsViewModel(@NonNull Application application) {
         super(application);
+        repository=new NewRepository(application);
     }
+
+    public LiveData<List<NewEntity>> getAllNews(){
+        return repository.getAllNews();
+    }
+
+    public void insert(NewEntity news){
+        repository.insert(news);
+    }
+
 }
