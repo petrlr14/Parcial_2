3fd31e26a55dcd6dd6e2f1718ab8830f540bfabd
petrlr14 <00057616@uca.edu.sv>
2018-06-02 23:52:15 -0600

    creacion de modelo completamente inecesaria

---
+++ app/src/main/java/com/pdm00057616/gamenews/models/Login.java
@@ -0,0 +1,31 @@
+package com.pdm00057616.gamenews.models;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Login {
+
+    @SerializedName("user")
+    private String username;
+    private String password;
+
+    public Login(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
