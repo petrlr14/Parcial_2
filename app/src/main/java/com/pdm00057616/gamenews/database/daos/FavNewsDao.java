7e33fd207bc109f4578b1cb04e2487d3f0b3e707
petrlr14 <00057616@uca.edu.sv>
2018-06-03 17:10:26 -0600

    se agrego notacion Dao

--- app/src/main/java/com/pdm00057616/gamenews/database/daos/FavNewsDao.java
+++ app/src/main/java/com/pdm00057616/gamenews/database/daos/FavNewsDao.java
@@ -1,9 +1,11 @@
 package com.pdm00057616.gamenews.database.daos;
 
+import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
 
 import com.pdm00057616.gamenews.database.entities_models.FavNewsEntity;
 
+@Dao
 public interface FavNewsDao {
 
     @Insert
