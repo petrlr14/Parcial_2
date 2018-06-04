74c939b3fe4a2c60875e06ca9a92f806ce439492
petrlr14 <00057616@uca.edu.sv>
2018-06-03 23:38:43 -0600

    se modifico logica para obtener mensaje de error

--- app/src/main/java/com/pdm00057616/gamenews/API/TokenDeserializer.java
+++ app/src/main/java/com/pdm00057616/gamenews/API/TokenDeserializer.java
@@ -5,18 +5,25 @@ import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
+import com.pdm00057616.gamenews.models.Login;
 
 import java.lang.reflect.Type;
 
-public class TokenDeserializer implements JsonDeserializer<String> {
+public class TokenDeserializer implements JsonDeserializer<Login> {
     @Override
-    public String deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-        String token="";
+    public Login deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        Login aux=new Login();
         System.out.println(json.getAsJsonObject());
         if(json.getAsJsonObject()!=null){
             JsonObject tokenJsonObject=json.getAsJsonObject();
-            token=tokenJsonObject.get("token").getAsString();
+            if (tokenJsonObject.get("token")!=null) {
+                aux.setToken(tokenJsonObject.get("token").getAsString());
+                aux.setOKResponse(true);
+            }else{
+                aux.setOKResponse(false);
+                aux.setToken(tokenJsonObject.get("message").getAsString());
+            }
         }
-        return token;
+        return aux;
     }
 }
