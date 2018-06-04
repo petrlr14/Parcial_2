181ce49be053937a7a4102e2521dc3ed0af9b3cd
petrlr14 <00057616@uca.edu.sv>
2018-06-04 12:23:35 -0600

    se agrego error en picasso

--- app/src/main/java/com/pdm00057616/gamenews/adapters/AllNewsAdapter.java
+++ app/src/main/java/com/pdm00057616/gamenews/adapters/AllNewsAdapter.java
@@ -1,6 +1,5 @@
 package com.pdm00057616.gamenews.adapters;
 
-import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -9,12 +8,8 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
-import com.bumptech.glide.request.RequestOptions;
 import com.pdm00057616.gamenews.R;
 import com.pdm00057616.gamenews.database.entities_models.NewEntity;
-import com.pdm00057616.gamenews.models.New;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
@@ -55,7 +50,7 @@ public class AllNewsAdapter extends RecyclerView.Adapter<AllNewsAdapter.ViewHold
                 news.getCoverImage().length() > 20) {
             Picasso.get().load(news.getCoverImage()).error(R.drawable.error_loading).into(holder.imageView);
         } else {
-            Picasso.get().load(R.drawable.error_loading).into(holder.imageView);
+            Picasso.get().load(R.drawable.error_loading).error(R.drawable.error_loading).into(holder.imageView);
         }
     }
 
