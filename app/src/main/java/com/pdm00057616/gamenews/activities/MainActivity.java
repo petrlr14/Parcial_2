810532746c2ddc02f4da7a5e3500f11bfc6e7369
petrlr14 <00057616@uca.edu.sv>
2018-06-04 22:23:31 -0600

    se configuro para agregar items al submenu dinamicamente

--- app/src/main/java/com/pdm00057616/gamenews/activities/MainActivity.java
+++ app/src/main/java/com/pdm00057616/gamenews/activities/MainActivity.java
@@ -1,5 +1,6 @@
 package com.pdm00057616.gamenews.activities;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -10,35 +11,36 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import com.pdm00057616.gamenews.API.ClientRequest;
 import com.pdm00057616.gamenews.R;
+import com.pdm00057616.gamenews.database.entities_models.CategoryEntity;
 import com.pdm00057616.gamenews.fragments.AllViewFragment;
+import com.pdm00057616.gamenews.viewmodels.CategoryViewModel;
 import com.squareup.picasso.Picasso;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
     private NavigationView navigationView;
     private DrawerLayout drawerLayout;
-
+    private CategoryViewModel categoryViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         isLogged();
-        if (Picasso.get() == null) {
-            Picasso picasso = new Picasso.
-                    Builder(getApplicationContext())
-                    .indicatorsEnabled(true)
-                    .build();
-            Picasso.setSingletonInstance(picasso);
-        }
-        ClientRequest.getCategories(getToken());
         setContentView(R.layout.activity_main);
+        categoryViewModel= ViewModelProviders.of(this).get(CategoryViewModel.class);
+        categoryViewModel
+                .getAllCategories()
+                .observe(this, this::addMenuItems);
+        ClientRequest.getCategories(getToken(),categoryViewModel);
         bindViews();
-        setFirstView();
     }
 
     @Override
@@ -58,29 +60,17 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_dehaze_24dp);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        navigationView.setNavigationItemSelectedListener((item) -> {
-                    Fragment fragment;
-                    switch (item.getItemId()) {
-                        default:
-                            fragment = new AllViewFragment();
-                            break;
-                    }
-                    getSupportFragmentManager().beginTransaction()
-                            .replace(R.id.frame_content, fragment)
-                            .commit();
-                    drawerLayout.closeDrawers();
-                    item.setChecked(true);
-                    return true;
-                }
-        );
+
     }
-    private void setFirstView(){
-        navigationView.getMenu().getItem(0).setChecked(true);
-        Fragment fragment = new AllViewFragment();
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.frame_content, fragment)
-                .commit();
-        getSupportActionBar().setTitle(navigationView.getMenu().getItem(0).getTitle());
+
+    private void addMenuItems(List<CategoryEntity> categories){
+        navigationView.getMenu().findItem(R.id.games_section).getSubMenu().clear();
+        for(CategoryEntity x:categories){
+            navigationView
+                    .getMenu().findItem(R.id.games_section)
+                    .getSubMenu().add(x.getName());
+            System.out.println("holi");
+        }
     }
 
     private void isLogged(){
