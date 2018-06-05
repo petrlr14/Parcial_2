887fc442f5ff0df44d2f00bc8dafcf492cdddb00
petrlr14 <00057616@uca.edu.sv>
2018-06-04 21:32:55 -0600

    clase modelo para player

---
+++ app/src/main/java/com/pdm00057616/gamenews/models/Player.java
@@ -0,0 +1,46 @@
+package com.pdm00057616.gamenews.models;
+
+public class Player {
+
+    private String avatar, _id, name, biografia, game;
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBiografia() {
+        return biografia;
+    }
+
+    public void setBiografia(String biografia) {
+        this.biografia = biografia;
+    }
+
+    public String getGame() {
+        return game;
+    }
+
+    public void setGame(String game) {
+        this.game = game;
+    }
+}
