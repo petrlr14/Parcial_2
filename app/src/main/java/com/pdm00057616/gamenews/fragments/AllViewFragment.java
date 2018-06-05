4925faf618cef094d593964eaa4d491500f34d53
petrlr14 <00057616@uca.edu.sv>
2018-06-04 21:33:17 -0600

    se quito llama a API

--- app/src/main/java/com/pdm00057616/gamenews/fragments/AllViewFragment.java
+++ app/src/main/java/com/pdm00057616/gamenews/fragments/AllViewFragment.java
@@ -20,28 +20,13 @@ import android.view.ViewGroup;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.gson.Gson;
-import com.pdm00057616.gamenews.API.GameNewsAPI;
+import com.pdm00057616.gamenews.API.ClientRequest;
 import com.pdm00057616.gamenews.R;
 import com.pdm00057616.gamenews.adapters.AllNewsAdapter;
-import com.pdm00057616.gamenews.database.entities_models.NewEntity;
-import com.pdm00057616.gamenews.models.New;
 import com.pdm00057616.gamenews.viewmodels.NewsViewModel;
 
 import java.lang.reflect.Field;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Collections;
-import java.util.List;
-import java.util.Locale;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class AllViewFragment extends Fragment {
 
@@ -78,55 +63,7 @@ public class AllViewFragment extends Fragment {
         });
         recyclerView.setLayoutManager(manager);
         recyclerView.setAdapter(adapter);
-        //init();
         return view;
-    }
-
-    private void init() {
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://gamenewsuca.herokuapp.com")
-                .addConverterFactory(GsonConverterFactory.create(new Gson()))
-                .build();
-        GameNewsAPI service = retrofit.create(GameNewsAPI.class);
-        Call<List<New>> news = service.getNews("Beared " + aux);
-        news.enqueue(new Callback<List<New>>() {
-            @Override
-            public void onResponse(Call<List<New>> call, Response<List<New>> response) {
-                if (response.isSuccessful()) {
-                    setListNewEntity(response.body());
-                    Toast.makeText(getContext(), "Fetching data", Toast.LENGTH_SHORT).show();
-                } else
-                    Toast.makeText(getContext(), response.message(), Toast.LENGTH_SHORT).show();
-
-            }
-
-            @Override
-            public void onFailure(Call<List<New>> call, Throwable t) {
-                Toast.makeText(getContext(), t.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
-    private void setListNewEntity(List<New> list) {
-        DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.US);
-        try {
-            for (New li : list) {
-                li.setCreatedDate(df.parse(li.getCreated_date()));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-
-        }
-        Collections.sort(list);
-        Collections.reverse(list);
-        for (New x : list) {
-            NewEntity newEntity = new NewEntity(
-                    x.get_id(), x.getTitle(), x.getCoverImage(), x.getDescription(),
-                    x.getBody(), x.getGame(), x.getCreated_date()
-            );
-            viewModel.insert(newEntity);
-        }
     }
 
     @Override
