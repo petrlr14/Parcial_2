fa9ad45cf4f1d2b5b2c8e24985e0b8ddf620afbc
petrlr14 <00057616@uca.edu.sv>
2018-06-05 21:41:13 -0600

    se agrego migracion 2_3, se agrego tabla player

--- app/src/main/java/com/pdm00057616/gamenews/database/AppDB.java
+++ app/src/main/java/com/pdm00057616/gamenews/database/AppDB.java
@@ -11,15 +11,17 @@ import android.support.annotation.NonNull;
 import com.pdm00057616.gamenews.database.daos.CategoryDao;
 import com.pdm00057616.gamenews.database.daos.FavNewsDao;
 import com.pdm00057616.gamenews.database.daos.NewDao;
+import com.pdm00057616.gamenews.database.daos.PlayerDao;
 import com.pdm00057616.gamenews.database.daos.UserDao;
 import com.pdm00057616.gamenews.database.entities_models.CategoryEntity;
 import com.pdm00057616.gamenews.database.entities_models.FavNewsEntity;
 import com.pdm00057616.gamenews.database.entities_models.NewEntity;
+import com.pdm00057616.gamenews.database.entities_models.PlayerEntity;
 import com.pdm00057616.gamenews.database.entities_models.UserEntity;
 
 @Database(
-        entities = {FavNewsEntity.class, NewEntity.class, UserEntity.class, CategoryEntity.class},
-        exportSchema = false, version = 2)
+        entities = { PlayerEntity.class,FavNewsEntity.class, NewEntity.class, UserEntity.class, CategoryEntity.class},
+        exportSchema = false, version = 3)
 public abstract class AppDB extends RoomDatabase {
 
     private static volatile AppDB db;
@@ -35,14 +37,21 @@ public abstract class AppDB extends RoomDatabase {
     private static AppDB createDB(Context context) {
         return Room.
                 databaseBuilder(context, AppDB.class, DB_NAME)
-                .addMigrations(MIGRATION_1_2).
-                build();
+                .addMigrations(MIGRATION_1_2, MIGRATION_2_3)
+                .build();
     }
 
     static final Migration MIGRATION_1_2 = new Migration(1, 2) {
         @Override
         public void migrate(@NonNull SupportSQLiteDatabase database) {
 
+        }
+    };
+    static final Migration MIGRATION_2_3 = new Migration(2, 3) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+            database.execSQL("CREATE TABLE PlayerEntity(id TEXT primary key not null," +
+                    "avatar TEXT, name TEXT, bio TEXT, game TEXT)");
         }
     };
 
@@ -53,5 +62,7 @@ public abstract class AppDB extends RoomDatabase {
     public abstract UserDao userDao();
 
     public abstract CategoryDao categoryDao();
+
+    public abstract PlayerDao playerDao();
 
 }
