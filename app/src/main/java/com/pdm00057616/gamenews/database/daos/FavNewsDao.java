9ef12bee3cc77862bc38cdbf3045fa784d245fb0
petrlr14 <00057616@uca.edu.sv>
2018-06-06 22:51:52 -0600

    se creo todo lo necesario para lidiar con favs

--- app/src/main/java/com/pdm00057616/gamenews/database/daos/FavNewsDao.java
+++ app/src/main/java/com/pdm00057616/gamenews/database/daos/FavNewsDao.java
@@ -1,14 +1,25 @@
 package com.pdm00057616.gamenews.database.daos;
 
+import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
 
 import com.pdm00057616.gamenews.database.entities_models.FavNewsEntity;
+
+import java.util.List;
 
 @Dao
 public interface FavNewsDao {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insertFavNew(FavNewsEntity favNew);
+
+    @Query("SELECT*FROM FavNewsEntity WHERE user_id=:user_id")
+    LiveData<List<FavNewsEntity>> getFavByUser(String user_id);
+
+    @Query("DELETE FROM FavNewsEntity where (user_id=:user and new_id=:news)")
+    void deleteFav(String user, String news);
 
 }
