0351be55ff14dcc40024ea349998bb5522a08895
petrlr14 <00057616@uca.edu.sv>
2018-06-11 08:56:33 -0600

    se creo actividad para detalles de noticias

---
+++ app/src/main/java/com/pdm00057616/gamenews/activities/DetailsActivity.java
@@ -0,0 +1,15 @@
+package com.pdm00057616.gamenews.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.pdm00057616.gamenews.R;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
