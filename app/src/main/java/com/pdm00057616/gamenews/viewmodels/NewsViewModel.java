d1abb96fcf48901f121e19771b7bf379db3fdedc
petrlr14 <00057616@uca.edu.sv>
2018-06-10 21:35:11 -0600

    se agrego metodo para hacer update para cambiar estado de favoritos

--- app/src/main/java/com/pdm00057616/gamenews/viewmodels/NewsViewModel.java
+++ app/src/main/java/com/pdm00057616/gamenews/viewmodels/NewsViewModel.java
@@ -6,7 +6,6 @@ import android.arch.lifecycle.LiveData;
 import android.content.Context;
 import android.support.annotation.NonNull;
 
-import com.pdm00057616.gamenews.API.ClientRequest;
 import com.pdm00057616.gamenews.database.entities_models.NewEntity;
 import com.pdm00057616.gamenews.database.repositories.NewRepository;
 
@@ -21,7 +20,7 @@ public class NewsViewModel extends AndroidViewModel {
         repository = new NewRepository(application);
     }
 
-    public LiveData<List<NewEntity>> getAllNews(Context context, String token) {
+    public LiveData<List<NewEntity>> getAllNews() {
         return repository.getAllNews();
     }
 
@@ -31,6 +30,10 @@ public class NewsViewModel extends AndroidViewModel {
 
     public LiveData<List<NewEntity>> getNewsByGame(String game) {
         return repository.getNewsByGame(game);
+    }
+
+    public void update(int fav, String id, String user, String token) {
+        repository.update(fav, id, user, token);
     }
 
     public void insert(NewEntity news) {
